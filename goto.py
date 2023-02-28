b7cb4a91f2ba969fed41fe82e8972bcf514c8e92
Paulo Rodriguez <paulo.f.rodriguez@gmail.com>
2023-02-27 21:03:35 -0300

    Dict mock added

--- goto.py
+++ goto.py
@@ -3,6 +3,12 @@ from markupsafe import escape
 import os
 
 
+urls = {
+     "google":"http://www.google.com",
+     "uol": "http://www.uol.com.br",
+     "gmail": "http://gmail.com"
+}
+
 app = Flask(__name__)
 
 
@@ -13,8 +19,13 @@ def index():
 
 @app.route('/<alias>')
 def goto(alias):
-     return redirect("http://www.google.com", code=302)
-
+     print(alias)
+     print(urls[alias])
+     #if (alias=='google'):
+          # return redirect("http://www.google.com", code=302)
+     return redirect(urls[alias], code=302)
+     #elif (alias=='uol'):
+     #     return redirect("http://www.uol.com", code=302)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
